44669ebf9d531602fc7d287e345ec49810daa1b5
Stewie154 <69080603+Stewie154@users.noreply.github.com>
2021-02-19 16:25:47 +0000

    Re-naming html elements
    Here I've re-named some html items more appropriately. Now I have this to point to fall back on, I'll continue working on the api call

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,13 +2,18 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
+    let formText = document.getElementById('text').value
+
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
+    
     fetch('http://localhost:3000/test')
+    //add key
+    // fetch('https://api.meaningcloud.com/sentiment-2.1?key=&of=json&txt=Main%20dishes%20were%20quite%20good%2C%20but%20desserts%20were%20too%20sweet%20for%20me.&model=Restaurants&lang=en')
     .then(res => res.json())
     .then(function(res) {
+        console.log(res)
         document.getElementById('results').innerHTML = res.message
     })
 }
