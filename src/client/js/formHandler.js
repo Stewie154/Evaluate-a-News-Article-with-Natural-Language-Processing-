0eedb50df10d2346ced6936fdf470587d5674f11
Stewie154 <69080603+Stewie154@users.noreply.github.com>
2021-02-19 17:22:36 +0000

    API response
    I'm now getting a response from the api call and I've started to add styling for displaying info to the user.
    
    In the next commit I'll continue with the styling and displaying info from the api call.
    
    NOTE: The api key is currently hard-coded in formhandler.js. After I get the display to where I want it, I'll work on retrieving the key from the node server instead (where it's stored.)
    
    For this reason I've removed the api key for the purposes of this commit. I'll add it back again and continue working on the next commit.

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -8,13 +8,14 @@ function handleSubmit(event) {
 
     console.log("::: Form Submitted :::")
     
-    fetch('http://localhost:3000/test')
+    // fetch('http://localhost:3000/test')
     //add key
-    // fetch('https://api.meaningcloud.com/sentiment-2.1?key=&of=json&txt=Main%20dishes%20were%20quite%20good%2C%20but%20desserts%20were%20too%20sweet%20for%20me.&model=Restaurants&lang=en')
+    fetch(`https://api.meaningcloud.com/sentiment-2.1?key=${ADD_API_KEY_HERE}&of=json&txt=${formText}&model=general&lang=en`)
     .then(res => res.json())
     .then(function(res) {
         console.log(res)
-        document.getElementById('results').innerHTML = res.message
+        document.getElementById('agreement').innerHTML = res.agreement
+
     })
 }
 
