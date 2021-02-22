b2fcd82fb683f8ab52fbbc985efe1ecee87ceaad
Stewie154 <69080603+Stewie154@users.noreply.github.com>
2021-02-22 10:31:50 +0000

    Api call display
    Again here I've removed the api key from the fetch request for the purpose of this commit.
    
    The results now display 3 different things from the api call to the user. Everything is working fine.
    
    I'll now work on retrieving the api key instead of hard coding it into the fetch request.

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -10,12 +10,14 @@ function handleSubmit(event) {
     
     // fetch('http://localhost:3000/test')
     //add key
-    fetch(`https://api.meaningcloud.com/sentiment-2.1?key=${ADD_API_KEY_HERE}&of=json&txt=${formText}&model=general&lang=en`)
+    fetch(`https://api.meaningcloud.com/sentiment-2.1?key=&of=json&txt=${formText}&model=general&lang=en`)
     .then(res => res.json())
     .then(function(res) {
         console.log(res)
+        //display results to user
         document.getElementById('agreement').innerHTML = res.agreement
-
+        document.getElementById('confidence').innerHTML = res.confidence
+        document.getElementById('irony').innerHTML = res.irony
     })
 }
 
