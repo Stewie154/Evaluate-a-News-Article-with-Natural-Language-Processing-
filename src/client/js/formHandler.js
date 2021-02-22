910549899311bbc293b1d04b0ffab211e4b1a096
Stewie154 <69080603+Stewie154@users.noreply.github.com>
2021-02-22 15:37:08 +0000

    Hidden api key
    Now the call to the external api is done without showing the apiKey. The call to the api is made in the express server via a https.get() method in the /submit route.
    
    This avoids displaying the api key in the client side js file.

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,24 +1,28 @@
-function handleSubmit(event) {
+const handleSubmit = async (event) => {
     event.preventDefault()
-
     // check what text was put into the form field
     let formText = document.getElementById('text').value
-
-    Client.checkForName(formText)
-
-    console.log("::: Form Submitted :::")
     
-    // fetch('http://localhost:3000/test')
-    //add key
-    fetch(`https://api.meaningcloud.com/sentiment-2.1?key=&of=json&txt=${formText}&model=general&lang=en`)
-    .then(res => res.json())
-    .then(function(res) {
-        console.log(res)
-        //display results to user
-        document.getElementById('agreement').innerHTML = res.agreement
-        document.getElementById('confidence').innerHTML = res.confidence
-        document.getElementById('irony').innerHTML = res.irony
-    })
+    try {
+        fetch('http://localhost:3000/submit?' + new URLSearchParams({
+            content: formText
+        }))
+        .then(res => res.json())
+        .then(function(res) {
+            console.log(res)
+            //display results to user
+            document.getElementById('agreement').innerHTML = res.agreement
+            document.getElementById('confidence').innerHTML = res.confidence
+            document.getElementById('irony').innerHTML = res.irony
+        })
+      
+
+    } catch (error) {
+        console.log('Error!', error)
+    }
+    
+    
+ 
 }
 
 export { handleSubmit }
