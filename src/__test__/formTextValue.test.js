ffc9366618834c673397400a6a0452a9b9e1aac0
Stewart <69080603+Stewie154@users.noreply.github.com>
2021-02-25 16:23:40 +0000

    Testing complete
    After reading up on the Udacity website, the complex tax of testing the main app has been deemed unnecessary.
    
    I have separated the grabbing of the input value into it's own function, and I've written a test for that.
    
    I also have a test for the display results function, they both passed.
    
    **NOTE: just in case I need it again later, I have moved the formHandler test file into the Fend-Webpack-Content repository. It has been removed from this project **

---
+++ src/__test__/formTextValue.test.js
@@ -0,0 +1,10 @@
+import {formTextValue} from '../client/js/formTextValue'
+
+test('Grabs value from input tag', () => {
+    //create mock dom element
+    document.body.innerHTML = '<input id="text" type="text" name="input" value="I eat apples" placeholder="Enter Text for Analysis:"/>'
+    //get input value by calling formTextValue function
+    const inputValue = formTextValue();
+    //Test to see input was grabbed correctly 
+    expect(inputValue).toEqual("I eat apples")
+})
---
+++ src/client/js/formTextValue.js
@@ -0,0 +1,4 @@
+export const formTextValue = () => {
+    let formTextValue = document.getElementById('text').value
+    return formTextValue;
+}
