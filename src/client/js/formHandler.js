95a5420ca1c070653acbd627b2c93467ec939a8d
Stewart <69080603+Stewie154@users.noreply.github.com>
2021-02-25 15:43:38 +0000

    Nearly solved formhandler test

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,23 +1,20 @@
 import {displayResults} from './displayResults'
-
 const handleSubmit = async (event) => {
-    event.preventDefault()
-    // check what text was put into the form field
     let formText = document.getElementById('text').value
-    
-    try {
+    event.preventDefault()
+    return new Promise( (resolve, reject) => {
         fetch('http://localhost:3000/submit?' + new URLSearchParams({
             content: formText
         }))
         .then(res => res.json())
         .then(function(res) {
-            console.log(res)
             displayResults(res)
+            console.log(2)
+            resolve()
         })
-
-    } catch (error) {
-        console.log('Error!', error)
-    }
+        .catch(reject)
+    })
 }
+
 
 export { handleSubmit }
